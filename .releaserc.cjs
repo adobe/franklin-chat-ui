9de6e3972723e53b1366fcceb8b14fb4a8b6e7f4
Alexandre Capt <acapt@adobe.com>
2023-03-15 14:55:31 +0100

    chore: add semantic release

---
+++ .releaserc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', {
+      'changelogFile': 'CHANGELOG.md',
+    }],
+    ["@semantic-release/npm", {
+      npmPublish: false,
+    }],
+    ['@semantic-release/exec', {
+      prepareCmd: 'npm run build',
+    }],
+    ['@semantic-release/git', {
+      'assets': ['package.json', 'package-lock.json', 'CHANGELOG.md', 'build/*'],
+      'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    }],
+    ['@semantic-release/github', {}],
+  ],
+  branches: ['main'],
+};
